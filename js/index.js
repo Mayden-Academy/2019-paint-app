130bfa8f43256ebffc7cac788cdea997c0284bd2
rrrichard <richard.rabulan@hotmail.co.uk>
2019-04-02 14:26:44 +0100

    added mouseUp and mouseDown event, added the paint ability with mousemove

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', function () {
 
     var canvas = document.querySelector('main')
+    var paintColor = '#FFFFFF'
+    var mouseIsPressed = false
 
     function createGrid() {
 
@@ -10,5 +12,22 @@ window.addEventListener('load', function () {
             div.classList.add('paintable')
         }
     }
+
+    canvas.addEventListener('mousedown', function (e) {
+        mouseIsPressed = true
+        var box = e.target
+        if (mouseIsPressed) {
+            box.style.backgroundColor = paintColor
+        }
+        canvas.addEventListener('mousemove', function (e) {
+            var box = e.target
+            if (mouseIsPressed) {
+                box.style.backgroundColor = paintColor
+            }
+        })
+    })
+    canvas.addEventListener('mouseup', function () {
+        mouseIsPressed = false
+    })
     createGrid()
 })
