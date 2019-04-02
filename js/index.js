9f627b3403150d443929df9af8b59788df651132
aldofiore28 <aldo.fiore95@gmail.com>
2019-04-02 15:55:48 +0100

    removed load event on page load

--- js/index.js
+++ js/index.js
@@ -1,36 +1,34 @@
-window.addEventListener('load', function () {
-    var canvas = document.querySelector('main')
-    var paintColor = '#FFFFFF'
-    var mouseIsPressed = false
+var canvas = document.querySelector('main')
+var paintColor = '#FFFFFF'
+var mouseIsPressed = false
 
-    function createGrid() {
-        for(var i = 0; i < 18644; i++ ) {
-            newDiv = document.createElement('div')
-            div = canvas.appendChild(newDiv)
-            div.classList.add('paintable')
-        }
+function createGrid() {
+    for(var i = 0; i < 18644; i++ ) {
+        newDiv = document.createElement('div')
+        div = canvas.appendChild(newDiv)
+        div.classList.add('paintable')
     }
+}
 
-    canvas.addEventListener('contextmenu', function () {
-        mouseIsPressed = false
-    })
+ canvas.addEventListener('contextmenu', function () {
+     mouseIsPressed = false
+ })
 
-    canvas.addEventListener('mousedown', function (e) {
-        e.preventDefault()
-        mouseIsPressed = true
+canvas.addEventListener('mousedown', function (e) {
+    e.preventDefault()
+    mouseIsPressed = true
+    var box = e.target
+    box.style.backgroundColor = paintColor
+
+    canvas.addEventListener('mousemove', function (e) {
         var box = e.target
-        box.style.backgroundColor = paintColor
-
-        canvas.addEventListener('mousemove', function (e) {
-            var box = e.target
-            if (mouseIsPressed) {
-                box.style.backgroundColor = paintColor
-            }
-        })
+        if (mouseIsPressed) {
+            box.style.backgroundColor = paintColor
+        }
     })
-
-    window.addEventListener('mouseup', function () {
-        mouseIsPressed = false
-    })
-    createGrid()
 })
+
+window.addEventListener('mouseup', function () {
+    mouseIsPressed = false
+})
+createGrid()
